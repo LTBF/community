b407fc11a899808cc8260abe53b356e0b7ab0416
Liu yz <1043457366@qq.com>
2020-09-17 22:44:26 +0800

    init repository

---
+++ src/main/java/com/example/community/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author shkstart
+ * @create 2020-09-17 22:13
+ */
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(@RequestParam(name = "name", defaultValue = "default")String name, Model model){
+        model.addAttribute("name", name);
+        return "test";
+    }
+}
